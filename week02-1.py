e462609fa52bb826c3aba685b05867fafda5f8bb
aquaqwq <qwqchuck@gmail.com>
2024-03-01 11:48:03 +0800

    week02

--- week02-1.py
+++
@@ -1,5 +0,0 @@
-N = len(s)
-one = 0
-for c in s:
-    if c == '1':one +=1
-return'1'*(one-1) + '0'*(N-one)+'1'
---
+++ week02/week02-1.py
@@ -0,0 +1,5 @@
+N = len(s)
+one = 0
+for c in s:
+    if c == '1':one +=1
+return'1'*(one-1) + '0'*(N-one)+'1'
